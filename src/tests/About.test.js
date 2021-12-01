7c97aeee0299cde1a76ba6fcf43d6408b0831550
severo <oliveira.severo@gmail.com>
2021-12-01 10:13:50 -0300

    Requisito 2 - Localizando imagem pelo endereco e pelo texto alternativo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 // import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
 import App from '../App';
 
 describe('Req 2 - Página About ', () => {
-  test('Verifica se pag About tem h2 com texto"About Pokédex"', () => {
+  test('Verifica se pag About contém h2 com texto"About Pokédex"', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/about');
 
     const sobrePokemons = screen.getByRole('heading',
       { level: 2, name: /About Pokédex/i });
     expect(sobrePokemons).toBeInTheDocument();
+  });
+
+  test('Verifica se pag About contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexImage = screen.getByRole('img');
+    expect(pokedexImage.alt).toBe('Pokédex');
+    expect(pokedexImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    // Imagem precisa de ter o texto Alternativo
+    // Imagem precisa te o link
   });
 });
