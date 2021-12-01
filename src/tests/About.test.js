2c897c0d0e83c3f3ef52c9e4b0f1b1db2261fff6
severo <oliveira.severo@gmail.com>
2021-12-01 10:47:20 -0300

    Requisito 2 - incluindo localizacao dos paragrafos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,9 +9,16 @@ describe('Req 2 - Página About ', () => {
   test('Verifica se pag About contém h2 com texto"About Pokédex"', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/about');
+    // Vou manter essa forma prara não esquecer que é viável
 
     const sobrePokemons = screen.getByRole('heading',
       { level: 2, name: /About Pokédex/i });
+    expect(sobrePokemons).toBeInTheDocument();
+  });
+
+  test('Verifica se pag About contém informacoes sobre a Pokedex"', () => {
+    renderWithRouter(<About />);
+    const sobrePokemons = screen.getByText(/About Pokédex/i);
     expect(sobrePokemons).toBeInTheDocument();
   });
 
@@ -20,7 +27,13 @@ describe('Req 2 - Página About ', () => {
     const pokedexImage = screen.getByRole('img');
     expect(pokedexImage.alt).toBe('Pokédex');
     expect(pokedexImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-    // Imagem precisa de ter o texto Alternativo
-    // Imagem precisa te o link
+  });
+
+  test('Verifica se pag About contém 2 parágrafos', () => {
+    renderWithRouter(<About />);
+    const p1 = screen.getByText(/This application/i);
+    const p2 = screen.getByText(/One can/i);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
   });
 });
