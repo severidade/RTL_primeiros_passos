6e31dbd72b25f9221ce16c19ab8138fa18a4d640
severo <oliveira.severo@gmail.com>
2021-12-01 09:55:10 -0300

    Iniciando Reqisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Req 2 - Página About ', () => {
+  test('Verifica se pag About tem h2 com texto"About Pokédex"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const sobrePokemons = screen.getByRole('heading',
+      { level: 2, name: /About Pokédex/i });
+    expect(sobrePokemons).toBeInTheDocument();
+  });
+});
