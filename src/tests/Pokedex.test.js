bf9e887426221ab794087dab441b53e6553347f7
severo <oliveira.severo@gmail.com>
2021-12-01 16:38:12 -0300

    Requisito 5 - Verifica se o botao next funciona

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 // import { fireEvent } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 // import Pokedex from '../components/Pokedex';
 import App from '../App';
@@ -12,5 +13,18 @@ describe('Req 5 - Testes em Pokedex.js', () => {
     const titlePokedex = screen.getByRole('heading',
       { level: 2, name: /Encountered pokémons/i });
     expect(titlePokedex).toBeInTheDocument();
+  });
+
+  test('Verifica exibição do próximo Pokémon ao clicar no botão próximo', () => {
+    // Entra na página do Pikachu
+    renderWithRouter(<App />);
+
+    // Clica em proximo pokemon
+    const nextPokemon = screen.getByTestId('next-pokemon', { text: /Próximo pokémon/i });
+    userEvent.click(nextPokemon);
+
+    // Verifica se Vai para o o pokemon Caterpie
+    const isCaterpie = screen.getByTestId('pokemon-name', { text: /Próximo Caterpie/i });
+    expect(isCaterpie).toBeDefined();
   });
 });
