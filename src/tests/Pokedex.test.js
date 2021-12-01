05873e774da7acc1efeb58d61fb565d098f50f08
severo <oliveira.severo@gmail.com>
2021-12-01 15:00:19 -0300

    Iniciando o requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+// import Pokedex from '../components/Pokedex';
+import App from '../App';
+
+describe('Req 5 - Testes em Pokedex.js', () => {
+  test('Verifica se página contem h2 com texto Page requested not found 😭', () => {
+    renderWithRouter(<App />);
+
+    const titlePokedex = screen.getByRole('heading',
+      { level: 2, name: /Encountered pokémons/i });
+    expect(titlePokedex).toBeInTheDocument();
+  });
+});
